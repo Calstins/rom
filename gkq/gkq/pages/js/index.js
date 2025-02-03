f39c77b5ceef78ab51712d62b2b9731d7cd207b1
Calstins <opulecalebtins@gmail.com>
2025-02-03 16:11:41 +0100

    first commit

---
+++ gkq/gkq/pages/js/index.js
@@ -0,0 +1,16 @@
+function goback() {
+  window.history.back();
+}
+
+function gocenter(){
+  window.location.href = "./center.html";
+}
+
+
+function gouserinfo(){
+  window.location.href = "./userinfo.html";
+}
+
+function gopassword(){
+  window.location.href = "./password.html";
+}
